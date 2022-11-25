cca9e47786e59bbf38fb8223534f3fbce2af0cd2
Annas0801 <annas0801@students.amikom.ac.id>
2022-11-25 19:55:00 +0800

    Project UTS

---
+++ UTS/Form1.Designer.vb
@@ -0,0 +1,349 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MenuUtama
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MenuUtama))
+        Me.pnlLeft = New System.Windows.Forms.Panel
+        Me.pnlContent1 = New System.Windows.Forms.Panel
+        Me.TextBox7 = New System.Windows.Forms.TextBox
+        Me.TextBox6 = New System.Windows.Forms.TextBox
+        Me.TextBox5 = New System.Windows.Forms.TextBox
+        Me.TextBox4 = New System.Windows.Forms.TextBox
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox
+        Me.btnOrder = New System.Windows.Forms.Button
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.btnMinimize = New System.Windows.Forms.Button
+        Me.btnMaximize = New System.Windows.Forms.Button
+        Me.btnClose = New System.Windows.Forms.Button
+        Me.btnStore = New System.Windows.Forms.Button
+        Me.btnSettings = New System.Windows.Forms.Button
+        Me.btnHistory = New System.Windows.Forms.Button
+        Me.btnHome = New System.Windows.Forms.Button
+        Me.pnlLeft.SuspendLayout()
+        Me.pnlContent1.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pnlLeft
+        '
+        Me.pnlLeft.BackColor = System.Drawing.Color.Gray
+        Me.pnlLeft.Controls.Add(Me.btnStore)
+        Me.pnlLeft.Controls.Add(Me.btnSettings)
+        Me.pnlLeft.Controls.Add(Me.btnHistory)
+        Me.pnlLeft.Controls.Add(Me.btnHome)
+        Me.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left
+        Me.pnlLeft.Location = New System.Drawing.Point(0, 0)
+        Me.pnlLeft.Name = "pnlLeft"
+        Me.pnlLeft.Size = New System.Drawing.Size(78, 768)
+        Me.pnlLeft.TabIndex = 0
+        '
+        'pnlContent1
+        '
+        Me.pnlContent1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.pnlContent1.Controls.Add(Me.TextBox7)
+        Me.pnlContent1.Controls.Add(Me.TextBox6)
+        Me.pnlContent1.Controls.Add(Me.TextBox5)
+        Me.pnlContent1.Controls.Add(Me.TextBox4)
+        Me.pnlContent1.Controls.Add(Me.TextBox3)
+        Me.pnlContent1.Controls.Add(Me.PictureBox3)
+        Me.pnlContent1.Controls.Add(Me.TextBox2)
+        Me.pnlContent1.Controls.Add(Me.PictureBox2)
+        Me.pnlContent1.Controls.Add(Me.NumericUpDown1)
+        Me.pnlContent1.Controls.Add(Me.btnOrder)
+        Me.pnlContent1.Controls.Add(Me.PictureBox1)
+        Me.pnlContent1.Controls.Add(Me.TextBox1)
+        Me.pnlContent1.Controls.Add(Me.btnMinimize)
+        Me.pnlContent1.Controls.Add(Me.btnMaximize)
+        Me.pnlContent1.Controls.Add(Me.btnClose)
+        Me.pnlContent1.Location = New System.Drawing.Point(75, 0)
+        Me.pnlContent1.Name = "pnlContent1"
+        Me.pnlContent1.Size = New System.Drawing.Size(7685, 768)
+        Me.pnlContent1.TabIndex = 1
+        '
+        'TextBox7
+        '
+        Me.TextBox7.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox7.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox7.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextBox7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox7.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox7.Location = New System.Drawing.Point(485, 182)
+        Me.TextBox7.Multiline = True
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(589, 133)
+        Me.TextBox7.TabIndex = 16
+        Me.TextBox7.Text = resources.GetString("TextBox7.Text")
+        '
+        'TextBox6
+        '
+        Me.TextBox6.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox6.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox6.Font = New System.Drawing.Font("Microsoft Yi Baiti", 65.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox6.ForeColor = System.Drawing.Color.White
+        Me.TextBox6.Location = New System.Drawing.Point(695, 57)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(10, 89)
+        Me.TextBox6.TabIndex = 15
+        Me.TextBox6.Text = "I"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox5.Font = New System.Drawing.Font("Microsoft Yi Baiti", 65.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox5.ForeColor = System.Drawing.Color.White
+        Me.TextBox5.Location = New System.Drawing.Point(575, 57)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(10, 89)
+        Me.TextBox5.TabIndex = 14
+        Me.TextBox5.Text = "I"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox4.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox4.Location = New System.Drawing.Point(719, 76)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(130, 64)
+        Me.TextBox4.TabIndex = 13
+        Me.TextBox4.Text = "Vegetable" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Category"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox3.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox3.Location = New System.Drawing.Point(599, 76)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(102, 64)
+        Me.TextBox3.TabIndex = 12
+        Me.TextBox3.Text = "23" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Ratings"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox2.Location = New System.Drawing.Point(467, 76)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(102, 64)
+        Me.TextBox2.TabIndex = 10
+        Me.TextBox2.Text = "4,8/5" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Average"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.NumericUpDown1.Location = New System.Drawing.Point(734, 349)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(37, 20)
+        Me.NumericUpDown1.TabIndex = 8
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(435, 43)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(145, 40)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "Brokoli"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.UTS.My.Resources.Resources.bintang__orange_
+        Me.PictureBox3.Location = New System.Drawing.Point(516, 76)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(29, 28)
+        Me.PictureBox3.TabIndex = 11
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.UTS.My.Resources.Resources.shopping_cart
+        Me.PictureBox2.Location = New System.Drawing.Point(669, 332)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(59, 53)
+        Me.PictureBox2.TabIndex = 9
+        Me.PictureBox2.TabStop = False
+        '
+        'btnOrder
+        '
+        Me.btnOrder.FlatAppearance.BorderSize = 0
+        Me.btnOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnOrder.Image = Global.UTS.My.Resources.Resources.button_getOrderNow_
+        Me.btnOrder.Location = New System.Drawing.Point(467, 321)
+        Me.btnOrder.Name = "btnOrder"
+        Me.btnOrder.Size = New System.Drawing.Size(196, 64)
+        Me.btnOrder.TabIndex = 7
+        Me.btnOrder.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.UTS.My.Resources.Resources.Screenshot_16_1
+        Me.PictureBox1.Location = New System.Drawing.Point(34, 47)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(403, 403)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'btnMinimize
+        '
+        Me.btnMinimize.FlatAppearance.BorderSize = 0
+        Me.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMinimize.Image = Global.UTS.My.Resources.Resources.minimize
+        Me.btnMinimize.Location = New System.Drawing.Point(945, 12)
+        Me.btnMinimize.Name = "btnMinimize"
+        Me.btnMinimize.Size = New System.Drawing.Size(33, 26)
+        Me.btnMinimize.TabIndex = 2
+        Me.btnMinimize.UseVisualStyleBackColor = True
+        '
+        'btnMaximize
+        '
+        Me.btnMaximize.FlatAppearance.BorderSize = 0
+        Me.btnMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMaximize.Image = Global.UTS.My.Resources.Resources.maximize
+        Me.btnMaximize.Location = New System.Drawing.Point(984, 12)
+        Me.btnMaximize.Name = "btnMaximize"
+        Me.btnMaximize.Size = New System.Drawing.Size(33, 26)
+        Me.btnMaximize.TabIndex = 1
+        Me.btnMaximize.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.FlatAppearance.BorderSize = 0
+        Me.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnClose.Image = Global.UTS.My.Resources.Resources.close
+        Me.btnClose.Location = New System.Drawing.Point(1023, 12)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(31, 26)
+        Me.btnClose.TabIndex = 0
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnStore
+        '
+        Me.btnStore.FlatAppearance.BorderSize = 0
+        Me.btnStore.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnStore.Image = Global.UTS.My.Resources.Resources.shopping_cart
+        Me.btnStore.Location = New System.Drawing.Point(12, 103)
+        Me.btnStore.Name = "btnStore"
+        Me.btnStore.Size = New System.Drawing.Size(54, 60)
+        Me.btnStore.TabIndex = 6
+        Me.btnStore.UseVisualStyleBackColor = True
+        '
+        'btnSettings
+        '
+        Me.btnSettings.FlatAppearance.BorderSize = 0
+        Me.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSettings.Image = Global.UTS.My.Resources.Resources.settings
+        Me.btnSettings.Location = New System.Drawing.Point(12, 169)
+        Me.btnSettings.Name = "btnSettings"
+        Me.btnSettings.Size = New System.Drawing.Size(54, 54)
+        Me.btnSettings.TabIndex = 5
+        Me.btnSettings.UseVisualStyleBackColor = True
+        '
+        'btnHistory
+        '
+        Me.btnHistory.FlatAppearance.BorderSize = 0
+        Me.btnHistory.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnHistory.Image = Global.UTS.My.Resources.Resources.time
+        Me.btnHistory.Location = New System.Drawing.Point(12, 242)
+        Me.btnHistory.Name = "btnHistory"
+        Me.btnHistory.Size = New System.Drawing.Size(54, 52)
+        Me.btnHistory.TabIndex = 4
+        Me.btnHistory.UseVisualStyleBackColor = True
+        '
+        'btnHome
+        '
+        Me.btnHome.FlatAppearance.BorderSize = 0
+        Me.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnHome.Image = Global.UTS.My.Resources.Resources.home
+        Me.btnHome.Location = New System.Drawing.Point(12, 47)
+        Me.btnHome.Name = "btnHome"
+        Me.btnHome.Size = New System.Drawing.Size(54, 50)
+        Me.btnHome.TabIndex = 3
+        Me.btnHome.UseVisualStyleBackColor = True
+        '
+        'MenuUtama
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1150, 768)
+        Me.Controls.Add(Me.pnlContent1)
+        Me.Controls.Add(Me.pnlLeft)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "MenuUtama"
+        Me.Padding = New System.Windows.Forms.Padding(0, 0, 15, 0)
+        Me.pnlLeft.ResumeLayout(False)
+        Me.pnlContent1.ResumeLayout(False)
+        Me.pnlContent1.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pnlLeft As System.Windows.Forms.Panel
+    Friend WithEvents pnlContent1 As System.Windows.Forms.Panel
+    Friend WithEvents btnMinimize As System.Windows.Forms.Button
+    Friend WithEvents btnMaximize As System.Windows.Forms.Button
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents btnHome As System.Windows.Forms.Button
+    Friend WithEvents btnStore As System.Windows.Forms.Button
+    Friend WithEvents btnSettings As System.Windows.Forms.Button
+    Friend WithEvents btnHistory As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents btnOrder As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+
+End Class
